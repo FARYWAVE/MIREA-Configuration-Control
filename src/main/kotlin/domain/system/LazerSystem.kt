2e21a0b1414fad36248fbd34f840bbe2b1990e5a
tfedorov <farywave.official@gmail.com>
2025-10-08 10:57:34 +0300

    Task1- final

--- src/main/kotlin/domain/system/LazerSystem.kt
+++ src/main/kotlin/domain/system/LazerSystem.kt
@@ -21,6 +21,7 @@ class LazerSystem {
             "     /___/"
 
     fun viewEnvironmentVariable(input: String): String {
+        if (input == "\$HOME") return System.getProperty("user.dir")
         val regex = "\\$[A-Za-z_][A-Za-z0-9_]*".toRegex()
         return regex.replace(input) { match ->
             val varName = match.value.substring(1)
